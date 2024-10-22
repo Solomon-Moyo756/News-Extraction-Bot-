fb98630ce80f9dd7f3631aab9d7a13afff8967ac
Solomon Moyo <solomon.moyo@dariel.co.za>
2024-10-22 03:07:15 +0200

    check if money is found and cout number of times a phrase is included

--- NewsExtractor.py
+++ NewsExtractor.py
@@ -4,6 +4,7 @@ from NewsSelenium import NewsSelenium
 from Article import NewsArticle
 import time
 from ElementIdentifiers import Identifier
+import re
 
 class NewsExtractor:
 
@@ -12,19 +13,19 @@ class NewsExtractor:
     def __init__(self,search_value) -> None:
         self.articles = []
         self.bot = NewsSelenium("https://gothamist.com/")
-
+        self.search_value = search_value
         self.bot.open_news_site()
         self.bot.wait_for_element(timeout=10,xpath_value=Identifier.donate_popup_xpath)
         self.bot.close_popup(popup_xpath=Identifier.donate_popup_xpath)
         time.sleep(2)
-        self.search_phrase(search_value)
+        self.search_the_phrase(self.search_value)
         #time.sleep(2)
         self.bot.wait_for_search_results(Identifier.css_selector_each_news_article_card)
         self.extract_news()
     
  
 
-    def search_phrase(self,search_phrase_value):
+    def search_the_phrase(self,search_phrase_value):
         #find the search I con and cclick it
         self.bot.wait_for_element(timeout=10,xpath_value=Identifier.search_icon_xpath)
         search_icon =self.bot.find_element_by_xpath(xpath_value= Identifier.search_icon_xpath)
@@ -66,18 +67,18 @@ class NewsExtractor:
 
                 try:
                     image_element = self.bot.find_element_by_xpath_from_element(from_element= a, xpath_value=Identifier.image_xpath)
-                    print(image_element)
                     picture_filepath = image_element.get_attribute('src')
                 except Exception as image_not_found:
                     picture_filepath = "no image path found"
-                #description = self.bot.find_element_by_xpath_from_element(from_element= card_slot, xpath_value='.//p[@class="desc"]').text
+                count_phrases = self.count_search_phrase(title=title,description=description)
+                is_money_mentioned = self.search_for_money(title=title, description=description)
                 article = NewsArticle(
                     title =title,
                     description = description,
                     date = date_published,
                     picture_filepath = picture_filepath,
-                    count_of_search_phrases = 3,
-                    is_money_mentioned = False
+                    count_of_search_phrases = count_phrases,
+                    is_money_mentioned = is_money_mentioned
                     )
                 
                 self.articles.append(article)
@@ -90,4 +91,25 @@ class NewsExtractor:
 
     def get_resultant_articles(self):
         return self.articles
+    
+    def search_for_money(self, title, description):
+        money_pattern = r'\b(money|\$|€|£|dollars|USD|ZAR)\b'
+        match_desc = re.search(money_pattern, description, re.IGNORECASE)
+        match_title = re.search(money_pattern, title, re.IGNORECASE)
+        if match_desc or match_title:
+            return True
+        else:
+            return False
+
+    def count_search_phrase(self,title, description):
+        arr_description_words = description.lower().split()
+        arr_title_words = title.lower().split()
+
+        count = 0
+        count += sum(1 for word in arr_description_words if self.search_value.lower() in word)
+        count += sum(1 for word in arr_title_words if self.search_value.lower() in word)
+        
+        return count
+
+
 
--- tasks.py
+++ tasks.py
@@ -4,11 +4,11 @@ from NewsExtractor import NewsExtractor
 import time
 
 if __name__ == "__main__":
-    news = NewsExtractor(search_value= "Kamala")
+    news = NewsExtractor(search_value= "Trump")
     results = news.get_resultant_articles()
     for i in range(len(results)):
-        print(f'title{i}: {results[i].title} \n descr: {results[i].description}\n date: {results[i].date}\n image: {results[i].picture_filepath}\n\n')
+        print(f'title{i}: {results[i].title} \n descr: {results[i].description}\n date: {results[i].date}\n image: {results[i].picture_filepath}\n count_of_search_phrases: {results[i].count_of_search_phrases}\n is_money_mentioned: {results[i].is_money_mentioned}\n\n')
         
-        #TODO: try rewview all the search results, meaning find a way to click the load more button and read all the n results
+        # TODO: try rewview all the search results, meaning find a way to click the load more button and read all the n results
         # Find the number of times the search phrase was included in the results
         # Find if the title and description include money in them...
